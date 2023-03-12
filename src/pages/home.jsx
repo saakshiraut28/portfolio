0681e1b87fe0a54eac14670974c47868ba936c98
saakshiraut28 <saakshiraut28@gmail.com>
2023-03-12 23:07:42 +0530

    Removed heading

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,13 +5,13 @@ function home() {
     return ( 
         <>  
   
-            <section className='content '>
+            <section className='content px-4'>
                     <div>
-                    <div className='grid place-items-center text-[50px]'>
+                    <div className='grid place-items-center text-center text-[50px]'>
                         <div><span className='font-lora'>I'm <span className='font-cat'>Saakshi Raut</span>.</span></div>
                     </div>
                     <div className='grid place-items-center h-[500px] text-[350px] font-mont font-black '>
-                        <div><span className='font-black text-transparent bg-clip-text bg-gradient-to-b from-[#FF0000] to-[#FFFFFF] tracking-wide animate-bounce'>HO</span><span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-[#2730FF] to-[#FFFFFF] tracking-wide'>LA</span></div>
+                        <div className='flex invisible md:visible'><span className='font-black text-transparent bg-clip-text bg-gradient-to-b from-[#FF0000] to-[#FFFFFF] tracking-wide'>HO</span><span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-[#2730FF] to-[#FFFFFF] tracking-wide'>LA</span></div>
                     </div>
                     </div>
                     <div className=''>
