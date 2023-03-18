1f88fd615d0cc7397a76cb80568cd3e7f940f227
saakshiraut28 <saakshiraut28@gmail.com>
2023-03-18 20:50:04 +0530

    Made a Preloader

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <nav className="flex justify-between py-12 px-12 sticky top-0 bg-white">
             <div className="h-[50px] w-[50px]">
-                <Link to='/'><img src={Paw} alt="Paws" /></Link>
+                <Link to='/'><span className="font-black text-2xl">&lt;/&gt;</span></Link>
             </div>
             <div className="font-nunito space-x-6 text-[18px] font-semibold">
                 <span className="line-through hover:underline"><Link to='/about'>About</Link></span>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,7 +8,7 @@ function home() {
             <section className='content px-4'>
                     <div>
                     <div className='grid place-items-center text-center text-[50px]'>
-                        <div><span className='font-lora'>I'm <span className='font-cat'>Saakshi Raut</span>.</span></div>
+                        <div><span className='font-lora'>I'm <span className=''>Saakshi Raut</span>.</span></div>
                     </div>
                     <div className='grid place-items-center h-[500px] text-[350px] font-mont font-black '>
                         <div className='flex invisible md:visible'><span className='font-black text-transparent bg-clip-text bg-gradient-to-b from-[#FF0000] to-[#FFFFFF] tracking-wide'>HO</span><span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-[#2730FF] to-[#FFFFFF] tracking-wide'>LA</span></div>
